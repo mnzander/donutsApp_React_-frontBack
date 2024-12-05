6810170930a9eb8eae7551b8d661f3efebdc5eac
mnzander <ander.munoz.rivas@zabalburu.org>
2024-12-05 14:46:24 +0100

    fix horizontal mobile view

--- donuts-manager/.next/static/chunks/webpack.js
+++ donuts-manager/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ce118d6e71bab532"; }
+/******/ 		__webpack_require__.h = function() { return "7c9f46ba4d36f8f5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
